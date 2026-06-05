de1f48df97eac49aebde7a8aec6fda23c76dc648
Mahtab Panahi <mahtabpnm@gmail.com>
2026-06-05 15:19:50 -0500

    Initial Selenium project setup (source + pom only)

---
+++ src/test/java/part1/LoginShouldFailTest.java
@@ -0,0 +1,49 @@
+package part1;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginShouldFailTest {
+    WebDriver driver; // control the browser and help to find the elements
+
+    @BeforeClass
+    public void setUp() {
+        // initialize the driver
+        // open the browser
+        // navigate to the URL
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+    @AfterClass // help to clean up our test
+
+    public void tearDown() {
+        if (driver != null) {
+          //  driver.quit();
+        }
+    }
+
+    @Test
+    public void testLoggingIntoApplication() throws InterruptedException {
+        Thread.sleep(2000);
+
+       WebElement username = driver.findElement(By.name("username"));
+       username.sendKeys("Admin");
+
+       WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("admin123");
+
+        driver.findElement(By.tagName("button")).click();
+        Thread.sleep(2000);
+        String actualResult = driver.findElement(By.tagName("h6")).getText();
+        String expectedResult = "Dashboard";
+        Assert.assertNotEquals(actualResult, expectedResult, "The user is not logged in successfully");
+
+    }
+}
