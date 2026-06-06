d68375d7bb4aaeb9088dd12f346eb0093363b109
Mahtab Panahi <mahtabpnm@gmail.com>
2026-06-05 20:49:28 -0500

    Login Tests have finish with POM

---
+++ src/main/java/com/saucedemo/pages/BasePage.java
@@ -0,0 +1,28 @@
+package com.saucedemo.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class BasePage {
+    // if we wanted to have a constructor, then it would be a good idea to have abstract
+
+    public static WebDriver driver;
+
+    public static void setDriver(WebDriver driver) {
+        BasePage.driver = driver;
+    }
+
+    protected WebElement find(By locator) {
+        return driver.findElement(locator);
+    }
+
+    protected void set(By locator, String text) {
+        find(locator).clear();
+        find(locator).sendKeys(text);
+    }
+
+    protected void click(By locator) {
+        find(locator).click();
+    }
+}
