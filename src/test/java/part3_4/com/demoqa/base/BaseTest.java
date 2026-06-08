a7c3f31ecb51165151c9bd1d44204a946f8dc7f4
Mahtab Panahi <mahtabpnm@gmail.com>
2026-06-07 19:28:35 -0500

    Fix BaseTest.java: make driver protected, use static final URL, remove unused import, call static setDriver correctly

--- src/test/java/part3_4/com/demoqa/base/BaseTest.java
+++ src/test/java/part3_4/com/demoqa/base/BaseTest.java
@@ -7,33 +7,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 public class BaseTest {
-    private WebDriver driver;
+    protected WebDriver driver;
     protected BasePage basePage;
     protected HomePage homePage;
 
-    private String DEMOQA_URL = "https://www.demoqa.com";
+    private static final String DEMOQA_URL = "https://www.demoqa.com";
 
     @BeforeClass
     public void setUp(){
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-
     }
 
     @BeforeMethod
-    //project will load application before each method
-
     public void loadApplication(){
         driver.get(DEMOQA_URL);
-        //create an instanse for base page
         basePage = new BasePage();
-        // seting driver
-        basePage.setDriver(driver);
-
-        // every page must go home page
+        BasePage.setDriver(driver);
         homePage = new HomePage();
     }
 
